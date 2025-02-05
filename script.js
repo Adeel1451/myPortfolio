0e00e4d4bcc64439643af94588f5f2867e29b3e9
Adeel1451 <adeelahmad1451@gmail.com>
2025-02-05 21:49:37 +0500

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,15 @@
 // Smooth Scroll Function
 function scrollToSection(id) {
-    document.getElementById(id).scrollIntoView({
-      behavior: 'smooth',
-      block: 'start',
-    });
-  }
-  
+  document.getElementById(id).scrollIntoView({
+    behavior: 'smooth',
+    block: 'start',
+  });
+}
+
+// Mobile Menu Toggle
+const mobileMenu = document.getElementById('mobile-menu');
+const navLinks = document.querySelector('.nav-links');
+
+mobileMenu.addEventListener('click', () => {
+  navLinks.classList.toggle('active');
+});
